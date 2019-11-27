00409a8d24ab21484498936da55dbbf2046a6612
Raul R. Negrean <raul.negrean@sap.com>
2019-11-27 10:11:50 -0500

    Implement the price handler

--- tap_occ_products/record/handler/price_handler.py
+++ tap_occ_products/record/handler/price_handler.py
@@ -4,6 +4,15 @@ from tap_occ_products.record.handler.decorators import Singleton
 
 @Singleton
 class PriceHandler(BaseHandler):
+    _code = 0
 
     def generate(self, price, **options):
-        return None
+        self._code += 1
+
+        return {
+            'code': self._code,
+            'currencyIso': price['currencyIso'],
+            'formattedValue': price['formattedValue'],
+            'priceType': price['priceType'],
+            'value': price['value']
+        }
