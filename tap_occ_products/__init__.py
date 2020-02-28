22180f8b2d23d82ae9cbc4aba528ed1dfeeaaa9e
Raul R. Negrean <raul.negrean@sap.com>
2020-02-28 11:38:18 -0500

    Fix schema issues

--- tap_occ_products/__init__.py
+++ tap_occ_products/__init__.py
@@ -63,11 +63,7 @@ def discover():
             stream_key_properties.append('sku')
 
         if schema_name == Record.PRICE_POINT.value:
-            updated_metadata = is_selected
-            updated_metadata['metadata']['valid-replication-keys'] = 'id'
-            updated_metadata['metadata']['replication-method'] = 'INCREMENTAL'
-
-            stream_metadata.append(updated_metadata)
+            stream_metadata.append(is_selected)
             stream_key_properties.append('id')
 
         if schema_name == Record.PRODUCT.value:
@@ -86,11 +82,7 @@ def discover():
             stream_key_properties.append('id')
 
         if schema_name == Record.STOCK_POINT.value:
-            updated_metadata = is_selected
-            updated_metadata['metadata']['valid-replication-keys'] = 'id'
-            updated_metadata['metadata']['replication-method'] = 'INCREMENTAL'
-
-            stream_metadata.append(updated_metadata)
+            stream_metadata.append(is_selected)
             stream_key_properties.append('id')
 
         catalog_entry = {
@@ -128,32 +120,88 @@ def sync(config, state, catalog):
     selected_stream_ids = get_selected_streams(catalog)
     timestamp = datetime.now(timezone.utc).isoformat()
 
-    for stream in catalog['streams']:
-        stream_id = stream['tap_stream_id']
-        stream_schema = stream['schema']
-        stream_key_properties = stream['key_properties']
-        if stream_id in selected_stream_ids:
-            LOGGER.debug('Writing {} schema: \n {} \n and key properties: {}'.format(
-                stream_id, stream_schema, stream_key_properties))
-            singer.write_schema(stream_id, stream_schema, stream_key_properties)
-        else:
-            LOGGER.warn('Ignoring unselected stream: {}'.format(stream_id))
+    if Record.CATEGORY.value in selected_stream_ids:
+        category_stream = \
+            next(filter(lambda stream: stream['tap_stream_id'] == Record.CATEGORY.value, catalog['streams']))
+        LOGGER.debug('Writing category schema: \n {} \n and key properties: {}'.format(
+            category_stream['schema'],
+            category_stream['key_properties']))
+        singer.write_schema(
+            category_stream['tap_stream_id'],
+            category_stream['schema'],
+            category_stream['key_properties'])
+
+    if Record.SPEC.value in selected_stream_ids:
+        spec_stream = \
+            next(filter(lambda stream: stream['tap_stream_id'] == Record.SPEC.value, catalog['streams']))
+        LOGGER.debug('Writing spec schema: \n {} \n and key properties: {}'.format(
+            spec_stream['schema'],
+            spec_stream['key_properties']))
+        singer.write_schema(
+            spec_stream['tap_stream_id'],
+            spec_stream['schema'],
+            spec_stream['key_properties'])
+
+    if Record.PRODUCT.value in selected_stream_ids:
+        product_stream = \
+            next(filter(lambda stream: stream['tap_stream_id'] == Record.PRODUCT.value, catalog['streams']))
+        LOGGER.debug('Writing product schema: \n {} \n and key properties: {}'.format(
+            product_stream['schema'],
+            product_stream['key_properties']))
+        singer.write_schema(
+            product_stream['tap_stream_id'],
+            product_stream['schema'],
+            product_stream['key_properties'])
+
+    if Record.PRODUCT_SPEC.value in selected_stream_ids:
+        product_spec_stream = \
+            next(filter(lambda stream: stream['tap_stream_id'] == Record.PRODUCT_SPEC.value, catalog['streams']))
+        LOGGER.debug('Writing product_spec schema: \n {} \n and key properties: {}'.format(
+            product_spec_stream['schema'],
+            product_spec_stream['key_properties']))
+        singer.write_schema(
+            product_spec_stream['tap_stream_id'],
+            product_spec_stream['schema'],
+            product_spec_stream['key_properties'])
+
+    if Record.CUSTOMER_SPECIFIC_PRICE.value in selected_stream_ids:
+        customer_specific_price_stream = \
+            next(filter(lambda stream: stream['tap_stream_id'] == Record.CUSTOMER_SPECIFIC_PRICE.value, catalog['streams']))
+        LOGGER.debug('Writing customer_specific_price schema: \n {} \n and key properties: {}'.format(
+            customer_specific_price_stream['schema'],
+            customer_specific_price_stream['key_properties']))
+        singer.write_schema(
+            customer_specific_price_stream['tap_stream_id'],
+            customer_specific_price_stream['schema'],
+            customer_specific_price_stream['key_properties'])
+
+    if Record.PRICE_POINT.value in selected_stream_ids:
+        price_point = \
+            next(filter(lambda stream: stream['tap_stream_id'] == Record.PRICE_POINT.value, catalog['streams']))
+        LOGGER.debug('Writing price_point schema: \n {} \n and key properties: {}'.format(
+            price_point['schema'],
+            price_point['key_properties']))
+        singer.write_schema(
+            price_point['tap_stream_id'],
+            price_point['schema'],
+            price_point['key_properties'])
+
+    if Record.STOCK_POINT.value in selected_stream_ids:
+        stock_point = \
+            next(filter(lambda stream: stream['tap_stream_id'] == Record.STOCK_POINT.value, catalog['streams']))
+        LOGGER.debug('Writing stock_point schema: \n {} \n and key properties: {}'.format(
+            stock_point['schema'],
+            stock_point['key_properties']))
+        singer.write_schema(
+            stock_point['tap_stream_id'],
+            stock_point['schema'],
+            stock_point['key_properties'])
 
     LOGGER.info('Fetching OCC products')
     products = OccClient(config).fetch_products()
 
     for product in products:
         LOGGER.info('Syncing product with code: {}'.format(product['code']))
-
-        price_point_record = \
-            build_record_handler(Record.PRICE_POINT).generate(product, timestamp=timestamp, tenant_id=TENANT_ID)
-        LOGGER.debug('Writing price_point record: {}'.format(price_point_record))
-        singer.write_record(Record.PRICE_POINT.value, price_point_record)
-
-        stock_point_record = \
-            build_record_handler(Record.STOCK_POINT).generate(product, timestamp=timestamp, tenant_id=TENANT_ID)
-        LOGGER.debug('Writing stock_point record: {}'.format(stock_point_record))
-        singer.write_record(Record.STOCK_POINT.value, stock_point_record)
 
         category_id = None
         if 'categories' in product:
@@ -180,10 +228,6 @@ def sync(config, state, catalog):
 
                 spec_id = None
                 for feature in classification['features']:
-                    # ignore specs without values
-                    if not feature.get('featureValues'):
-                        continue
-
                     # ignore specs with multiple values
                     if len(feature.get('featureValues')) > 1:
                         continue
@@ -205,6 +249,15 @@ def sync(config, state, catalog):
                     LOGGER.debug('Writing product spec record: {}'.format(product_spec_record))
                     singer.write_record(Record.PRODUCT_SPEC.value, product_spec_record)
 
+        price_point_record = \
+            build_record_handler(Record.PRICE_POINT).generate(product, timestamp=timestamp, tenant_id=TENANT_ID)
+        LOGGER.debug('Writing price_point record: {}'.format(price_point_record))
+        singer.write_record(Record.PRICE_POINT.value, price_point_record)
+
+        stock_point_record = \
+            build_record_handler(Record.STOCK_POINT).generate(product, timestamp=timestamp, tenant_id=TENANT_ID)
+        LOGGER.debug('Writing stock_point record: {}'.format(stock_point_record))
+        singer.write_record(Record.STOCK_POINT.value, stock_point_record)
     return
 
 
