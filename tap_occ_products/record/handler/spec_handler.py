872268962c5859bf8b1d2814d9c4c022be8fcf66
Raul R. Negrean <raul.negrean@sap.com>
2020-04-01 21:08:24 -0400

    Fix spec handler code issue

--- tap_occ_products/record/handler/spec_handler.py
+++ tap_occ_products/record/handler/spec_handler.py
@@ -8,13 +8,18 @@ class SpecHandler(BaseHandler):
     _code = 0
 
     def generate(self, spec, **options):
-        if spec.get('code') in self._handled_codes:
-            return self._handled_codes.get(spec.get('code'))
+        if ',' in spec.get('code'):
+            code = spec.get('code').split(',')[1].strip()
+        else:
+            code = spec.get('code')
+
+        if code in self._handled_codes:
+            return self._handled_codes.get(code)
 
         self._code += 1
         spec_id = options.get('tenant_id') + str(self._code)
 
-        self._handled_codes[spec['code']] = spec_id
+        self._handled_codes[code] = spec_id
 
         return {
             'id': spec_id,
