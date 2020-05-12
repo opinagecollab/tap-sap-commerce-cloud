fdedbdcbb601d50c16c05a16dcdef8ea8b650667
Raul R. Negrean <raul.negrean@sap.com>
2020-05-12 13:53:13 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="tap-sap-commerce-cloud",
     version="0.1.0",
     description="Singer.io tap for extracting data",
-    author="Stitch",
+    author="SAP and Stitch",
     url="http://singer.io",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
-    py_modules=["tap_sap_commerce_cloud"],
+    py_modules=[
+        "tap_sap_commerce_cloud.client",
+        "tap_sap_commerce_cloud.record",
+        "tap_sap_commerce_cloud.record.handler"
+    ],
     install_requires=[
         "singer-python==5.6.1",
         "requests==2.20.0",
@@ -17,9 +21,9 @@ setup(
     [console_scripts]
     tap-sap-commerce-cloud=tap_sap_commerce_cloud:main
     """,
-    packages=["tap_sap_commerce_cloud"],
+    packages=find_packages(),
     package_data = {
         "schemas": ["tap_sap_commerce_cloud/schemas/*.json"]
     },
-    include_package_data=True
+    include_package_data=True,
 )
